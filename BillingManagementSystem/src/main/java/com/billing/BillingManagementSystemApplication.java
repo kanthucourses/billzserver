c7adc338ac7d283e6db39653e21606e5333319f8
kanthucourses <kanthucourses@gmail.com>
2023-12-11 22:55:39 +0530

    main class

--- BillingManagementSystem/src/main/java/com/billing/BillingManagementSystemApplication.java
+++ BillingManagementSystem/src/main/java/com/billing/BillingManagementSystemApplication.java
@@ -2,9 +2,10 @@ package com.billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BillingManagementSystemApplication {
+public class BillingManagementSystemApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingManagementSystemApplication.class, args);
